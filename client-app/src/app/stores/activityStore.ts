1e5b1f388c01b23371ca27467f0974028c606e2b
Abdul Badru <abdul.badru@gmail.com>
2019-12-05 19:27:34 +0000

    End of Section 12

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,16 +1,21 @@
-import {action, observable, computed, configure, runInAction} from "mobx";
-import {createContext, SyntheticEvent} from "react";
+import {action, observable, computed, runInAction} from "mobx";
+import {SyntheticEvent} from "react";
 import {IActivity} from "../models/activity";
 import agent from "../api/agent";
 import {history} from "../../index";
 import {toast} from "react-toastify";
+import {RootStore} from "./rootStore";
 
 
-configure({
-    enforceActions: "always"
-});
 
-class ActivityStore {
+
+export default class ActivityStore {
+
+    rootStore: RootStore;
+    constructor(rootStore: RootStore) {
+        this.rootStore = rootStore;
+    }
+
     @observable activityRegistry = new Map();
     @observable activity: IActivity | null = null;
     @observable loadingInitial = false;
@@ -154,4 +159,3 @@ class ActivityStore {
 
 }
 
-export default createContext(new ActivityStore());
---
+++ client-app/src/features/user/RegisterForm.tsx
@@ -0,0 +1,50 @@
+import React, {useContext} from "react";
+
+import {Form as FinalForm, Field} from 'react-final-form';
+import {Button, Form, Header} from "semantic-ui-react";
+import TextInput from "../../app/common/form/TextInput";
+import {RootStoreContext} from "../../app/stores/rootStore";
+import {IUserFormValues} from "../../app/models/user";
+import {FORM_ERROR} from "final-form";
+import {combineValidators, isRequired} from "revalidate";
+import ErrorMessage from "../../app/common/form/ErrorMessage";
+
+const validate = combineValidators({
+    email: isRequired('email'),
+    password: isRequired('password'),
+    username: isRequired('username'),
+    displayName: isRequired('display name')
+})
+
+const RegisterForm = () => {
+
+    const rootStore = useContext(RootStoreContext);
+
+    const {register} = rootStore.userStore;
+
+
+    return (
+        <FinalForm
+            onSubmit={(values: IUserFormValues) => register(values).catch(error => ({
+                [FORM_ERROR]: error
+            }))}
+            validate={validate}
+            render={({handleSubmit, submitting, submitError, invalid, pristine, dirtyFieldsSinceLastSubmit}) => (
+                <Form onSubmit={handleSubmit} error>
+                    <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center'/>
+                    <Field name='email' component={TextInput} placeholder='Email'/>
+                    <Field name='username' component={TextInput} placeholder='Username'/>
+                    <Field name='displayName' component={TextInput} placeholder='Display Name'/>
+                    <Field name='password' component={TextInput} placeholder='Password' type='password'/>
+                    {submitError && !dirtyFieldsSinceLastSubmit &&
+                    <ErrorMessage error={submitError} text=''/>}
+                    <br/>
+                    <Button fluid loading={submitting} color='teal' content='Login'
+                            disabled={invalid && !dirtyFieldsSinceLastSubmit || pristine}/>
+                </Form>
+            )}
+        />
+    )
+}
+
+export default RegisterForm;
